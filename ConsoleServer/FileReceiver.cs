625d3a43d18a31554d5512ebaa20462bc27539ee
yjono <pek9dkl@gmail.com>
2017-11-10 02:05:46 +0900

    Initial Commit

---
+++ ConsoleServer/FileReceiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    public class FileReceiver
+    {
+        public void Receive()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://+:8123/");
+            listener.Start();
+            StreamWriter writer = new StreamWriter(@"Test.txt");
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerResponse res = context.Response;
+                switch (context.Request.Url.AbsolutePath)
+                {
+                    case "/test.cgi":
+                        {
+                            StreamReader reader = new StreamReader(context.Request.InputStream);
+                            string str = reader.ReadToEnd();
+                            writer.WriteLine(str);
+                            writer.Flush();
+                        }
+                        break;
+                }
+                byte[] html = System.Text.Encoding.GetEncoding(932).GetBytes(@"<form action='test.cgi' method='post'><input type='text' name='name'></form>");
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.ContentType = "text/html";
+                context.Response.OutputStream.Write(html, 0, html.Length);
+                res.Close();
+            }
+        }
+    }
+}
---
+++ WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //送信するファイルのパス
+            string filePath = @"C:\Users\pek9d\Desktop\IMG_5498.JPG";
+            //送信先のURL
+            string url = "http://127.0.0.1:8080/ListenerTest/";
+
+            WebClient wc = new WebClient();
+            try
+            {
+                //データを送信し、また受信する
+                byte[] resData = wc.UploadFile(url, filePath);
+                //受信したデータを表示する
+                string resText = Encoding.UTF8.GetString(resData);
+                Console.WriteLine(resText);
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
